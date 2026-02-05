e0357b1186a9819c2346de5267fb4c0446929754
JulianScipioni06 <scipionijulian@gmail.com>
2026-02-05 11:34:37 -0300

    Prueba Cronometro

--- script.js
+++ script.js
@@ -376,3 +376,53 @@ function imprimirTicket(index) {
     ventanaImpresion.document.write(contenidoTicket);
     ventanaImpresion.document.close();
 }
+
+//Probando Cronometrooo
+let horas = 0;
+let minutos = 0;
+let segundos = 0;
+let intervalo = null;
+let estaCorriendo = false;
+
+function cronometro() {
+    segundos++;
+
+    if (segundos === 60) {
+        segundos = 0;
+        minutos++;
+        if (minutos === 60) {
+            minutos = 0;
+            horas++;
+        }
+    }
+
+    // Formateo de ceros
+    let h = horas < 10 ? "0" + horas : horas;
+    let m = minutos < 10 ? "0" + minutos : minutos;
+    let s = segundos < 10 ? "0" + segundos : segundos;
+
+    document.getElementById("tiempo").innerHTML = `${h}:${m}:${s}`;
+}
+
+function iniciar() {
+    if (!estaCorriendo) {
+        intervalo = setInterval(cronometro, 1000);
+        estaCorriendo = true;
+    }
+}
+
+function pausar() {
+    if (estaCorriendo) {
+        clearInterval(intervalo);
+        estaCorriendo = false;
+    }
+}
+
+function reiniciar() {
+    clearInterval(intervalo);
+    estaCorriendo = false;
+    horas = 0;
+    minutos = 0;
+    segundos = 0;
+    document.getElementById("tiempo").innerHTML = "00:00:00";
+}
